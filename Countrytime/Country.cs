b4af7bb56a8abc5d0825d6cd0a063ed850f744e2
Віктор Липкань <lipkan02@gmail.com>
2022-07-09 22:35:01 +0300

    Добавьте файлы проекта.

---
+++ Countrytime/Country.cs
@@ -0,0 +1,18 @@
+﻿class Country
+{
+    public string Name { get; set; }
+    public string Capital { get; set; }
+    public string Language { get; set; }
+    public int CountryId { get; set; }
+    public DateTime Time { get; set; }
+    public void Print()
+    {
+        Console.WriteLine($"Country:\t {Name}");
+        Console.WriteLine($"Capital:\t {Capital}");
+        Console.WriteLine($"Language:\t {Language}");
+        Console.WriteLine($"Id:\t \t {CountryId}");
+        Console.WriteLine($"Time:\t  \t {Time:HH:mm:ss}");
+        Console.WriteLine("==============================");
+    }   
+}
+
---
+++ Countrytime/Program.cs
@@ -0,0 +1,19 @@
+﻿string country = Console.ReadLine();
+
+Country ukraine = new()
+{
+    Name = "Ukraine",
+    Capital = "Kyiv",
+    Language = "Ukraining",
+    CountryId = +380,
+    Time = DateTime.UtcNow.AddHours(3)
+};
+Country poland = new()
+{
+    Name = "Poland",
+    Capital = "Warsaw",
+    Language = "Polish",
+    CountryId = +48,
+    Time = DateTime.UtcNow.AddHours(2)
+};
+ukraine.Print();
